0582668b12777634d66e69792c8985713d9ae158
NightShaver <felixalexanderpuetz@gmail.com>
2026-04-14 15:52:53 +0200

    hs

---
+++ hideout/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Ali'
+version '1.0.0'
+this_is_a_map 'yes'
+
+files {
+
+    'stream/ali_generator.ytyp'
+}
+
+
+
+data_file 'DLC_ITYP_REQUEST' 'stream/ali_generator.ytyp'
